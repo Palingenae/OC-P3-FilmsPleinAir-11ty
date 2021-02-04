ca5f74fa5483c6e4668ba8a1028dce920edeaf70
Olivia De Scheerder <olivia.descheerder@knplabs.com>
2021-02-04 17:46:28 +0100

    Update : mobile nav implementation

--- assets/javascript/navigation.js
+++ assets/javascript/navigation.js
@@ -1,8 +1,22 @@
-const navMenu = document.querySelector('.navigation__menu--mobile');
-const iconMenu = document.querySelector('.icon--menu--mobile');
+console.log(document);
 
-function toggleMenu() {
-    if (iconMenu.classList.contains(".icon--menu--mobile--active")) {
-        iconMenu.classList.replace("")
+function displayMenu() {
+    if (document.documentElement.clientWidth < 960) {
+    const menuToggle = document.getElementById("menuToggle");
+
+    menuToggle.addEventListener("click", function() {
+        const drawerMenu = document.getElementById('drawerMenu');
+        drawerMenu.classList.add("navigation__menu--mobile--hidden");
+    });
     }
 }
+
+
+
+
+// const closeButton = document.querySelector(".button--action--close");
+
+// closeButton.addEventListener("click", function() {
+//     const modal = document.getElementById('modal');
+//     modal.classList.add("modal--hidden");
+// });
