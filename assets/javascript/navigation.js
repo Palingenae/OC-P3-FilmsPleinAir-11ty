4adee9456ee16824e21abd0cb6db2eabe2f65e6d
Olivia De Scheerder <olivia.descheerder@knplabs.com>
2021-02-04 14:03:20 +0100

    Update : navigation

---
+++ assets/javascript/navigation.js
@@ -0,0 +1,8 @@
+const navMenu = document.querySelector('.navigation__menu--mobile');
+const iconMenu = document.querySelector('.icon--menu--mobile');
+
+function toggleMenu() {
+    if (iconMenu.classList.contains(".icon--menu--mobile--active")) {
+        iconMenu.classList.replace("")
+    }
+}
