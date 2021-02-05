cd63ad1ccad3cce4796e6948e507a10c077c6dc7
Olivia De Scheerder <olivia.descheerder@knplabs.com>
2021-02-05 11:36:34 +0100

    Feature : display mobile menu

--- assets/javascript/navigation.js
+++ assets/javascript/navigation.js
@@ -1,17 +1,17 @@
-console.log(document);
-
 function displayMenu() {
     if (document.documentElement.clientWidth < 960) {
         const menuToggle = document.getElementById("menuToggle");
         const drawerMenu = document.getElementById('drawerMenu');
 
         menuToggle.addEventListener("click", function() {
-            drawerMenu.classList.remove("navigation__menu--mobile--active");
-        }, false);
-
-        menuToggle.addEventListener("click", function() {
             drawerMenu.classList.add("navigation__menu--mobile--active");
         }, false);
+
+        while (menuToggle.classList.contains("navigation__menu--mobile--active")) {
+            menuToggle.addEventListener("click", function() {
+                drawerMenu.classList.remove("navigation__menu--mobile--active");
+            }, false);
+        }
 
     }
 }
